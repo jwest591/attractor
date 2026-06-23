e1b8d157fb5ca608eb555715012f012d1a56e44e
Jon <jwest591@github.com>
2026-06-23 17:13:44 +0000

    Fix incorrect context report

--- cli/scripts/att-status-line.sh
+++ cli/scripts/att-status-line.sh
@@ -6,7 +6,7 @@ set -u
 
 INPUT="$(cat)"
 
-pct_raw=$(printf '%s' "$INPUT" | jq -r '.context_window.percent // 0' 2>/dev/null)
+pct_raw=$(printf '%s' "$INPUT" | jq -r '.context_window.used_percentage // 0' 2>/dev/null)
 if [ $? -ne 0 ] || [ -z "$pct_raw" ] || [ "$pct_raw" = "null" ]; then
     pct=0
     pct_display="??"
@@ -14,7 +14,12 @@ else
     pct="$pct_raw"
     pct_display="$pct_raw"
 fi
-total_raw=$(printf '%s' "$INPUT" | jq -r '.context_window.current_usage.input_tokens // 0' 2>/dev/null)
+total_raw=$(printf '%s' "$INPUT" | jq -r '
+    ((.context_window.current_usage.input_tokens // 0) +
+     (.context_window.current_usage.cache_creation_input_tokens // 0) +
+     (.context_window.current_usage.cache_read_input_tokens // 0) +
+     (.context_window.current_usage.output_tokens // 0))
+' 2>/dev/null)
 if [ $? -ne 0 ] || [ -z "$total_raw" ] || [ "$total_raw" = "null" ]; then
     total=0
 else
